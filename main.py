7c484e99162ea261a67f827dbaf9fa866dcd9b26
ASDleves <ricsi20020302@gmail.com>
2023-01-31 10:49:59 +0100

    érték kiszámitása metodussal

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 #megoldás a metodus alá
+def pontszamitas(gep_lap,jatekos_lap):
+    gepertek = 0
+    jatekosertek = 0
+    for i in range(len(gep_lap)):
+        gepertek += gep_lap[i]
+    for i in range(len(jatekos_lap)):
+        gepertek += jatekos_lap[i]
 
-def eredmeny(gep_pont,jatekos_pont):
-    if gep_pont > 21:
+
+def eredmeny(gepertek,jatekosertek):
+    if gepertek > 21:
         print("A gép vesztett")
-    elif jatekos_pont > 21:
+    if jatekosertek > 21:
         print("A játékos vesztett")
 
 #tesztesetek
