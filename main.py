0cf6cb7ff2a2a9a2beffed4b2273eb4131be3d09
ASDleves <ricsi20020302@gmail.com>
2023-01-31 10:24:08 +0100

    pontrendszer elágazás

---
+++ main.py
@@ -0,0 +1,9 @@
+#megoldás a metodus alá
+
+def eredmeny(gep_pont,jatekos_pont):
+    if gep_pont > 21:
+        print("A gép vesztett")
+    elif jatekos_pont > 21:
+        print("A játékos vesztett")
+
+#tesztesetek
