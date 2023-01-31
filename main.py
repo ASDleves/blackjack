7522ba7a34dbe52896fb2c5247b254f529fac46d
ASDleves <ricsi20020302@gmail.com>
2023-01-31 11:51:01 +0100

    teszteset, tesztmetodus

--- main.py
+++ main.py
@@ -9,9 +9,29 @@ def pontszamitas(lapok):
 def eredmeny(gep_lapok,jatekos_lapok):
     gepertek = pontszamitas(gep_lapok)
     jatekosertek = pontszamitas(jatekos_lapok)
+    eredmeny = ""
     if gepertek > 21:
-        print("A gép vesztett")
-    if jatekosertek > 21:
-        print("A játékos vesztett")
+        eredmeny = "A gép vesztett"
+    elif jatekosertek > 21:
+        eredmeny = "A játékos vesztett"
+    else:
+        eredmeny = "ház vesztett"
+    return eredmeny
+
+def jatekosvesztett_teszt():
+    jatekos = [10,10,5]
+    gep = [5,10]
+    vart_eredmeny = "A játékos vesztett"
+    kapott_eredmeny = eredmeny(jatekos,gep)
+    if kapott_eredmeny == vart_eredmeny:
+        print("teszt sikeres")
+    else:
+        print("teszt megbukott")
+
+def teszt_tesztesetek():
+    jatekosvesztett_teszt()
+teszt_tesztesetek()
+
+
 
 #tesztesetek
