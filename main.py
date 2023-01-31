fbdb3bed7ed0ee3923d1d98be35de533b2732cf8
ASDleves <ricsi20020302@gmail.com>
2023-01-31 11:07:25 +0100

    pont szamitas 1 valtozoval

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #megoldás a metodus alá
-def pontszamitas(gep_lap,jatekos_lap):
-    gepertek = 0
-    jatekosertek = 0
-    for i in range(len(gep_lap)):
-        gepertek += gep_lap[i]
-    for i in range(len(jatekos_lap)):
-        gepertek += jatekos_lap[i]
+def pontszamitas(lapok):
+    ertek = 0
+    for i in range(len(lapok)):
+        ertek += lapok[i]
 
+    return ertek
 
-def eredmeny(gepertek,jatekosertek):
+def eredmeny(gep_lapok,jatekos_lapok):
+    gepertek = pontszamitas(gep_lapok)
+    jatekosertek = pontszamitas(jatekos_lapok)
     if gepertek > 21:
         print("A gép vesztett")
     if jatekosertek > 21:
