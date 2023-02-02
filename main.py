3163d506db9f93c17de1a130fb2c7d7d5f95bc89
ASDleves <ricsi20020302@gmail.com>
2023-02-02 15:46:27 +0100

    tobb teszteset elkeszulese

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ def eredmeny(jatekos_lapok,gep_lapok):
         eredmenykiir = "A gép vesztett"
     elif jatekosertek > 21:
         eredmenykiir = "A játékos vesztett"
+    elif gepertek > jatekosertek and gepertek <21:
+        eredmenykiir = "A játékos vesztett"
+    elif gepertek == jatekosertek and gepertek <21 and len(gep_lapok) < len(jatekos_lapok):
+        eredmenykiir = "A játékos vesztett"
     else:
         eredmenykiir = "ház vesztett"
     return eredmenykiir
 
-def jatekosvesztett_teszt():
+def jatekosvesztettnagyobbertekkel_teszt():
     jatekos_lapok = [10,10,5,]
     gep_lapok = [5,10]
     vart_eredmeny = "A játékos vesztett"
@@ -26,10 +30,32 @@ def jatekosvesztett_teszt():
         print("teszt sikeres")
     else:
         print("teszt megbukott")
+def jatekosvesztettkisebbertekkel_teszt():
+    jatekos_lapok = [10,5]
+    gep_lapok = [10,10]
+    vart_eredmeny = "A játékos vesztett"
+    kapott_eredmeny = eredmeny(jatekos_lapok,gep_lapok)
+    if kapott_eredmeny == vart_eredmeny:
+        print("teszt sikeres")
+    else:
+        print("teszt megbukott")
+def jatekosvesztettkevesebblappal_teszt():
+    jatekos_lapok = [10,5,5]
+    gep_lapok = [10,10]
+    vart_eredmeny = "A játékos vesztett"
+    kapott_eredmeny = eredmeny(jatekos_lapok,gep_lapok)
+    if kapott_eredmeny == vart_eredmeny:
+        print("teszt sikeres")
+    else:
+        print("teszt megbukott")
 
 def teszt_tesztesetek():
-    print("A játékos vesztett teszt: ", end="")
-    jatekosvesztett_teszt()
+    print("A játékos vesztett teszt(nagyobb ertekkel): ", end="")
+    jatekosvesztettnagyobbertekkel_teszt()
+    print("A játékos vesztett teszt (kisebb értékkel): ",end="")
+    jatekosvesztettkisebbertekkel_teszt()
+    print("A játékos vesztett teszt (kevesebb lappal): ",end="")
+    jatekosvesztettkevesebblappal_teszt()
 
 teszt_tesztesetek()
 
